2b9d2d7ed872c870d68bf2d8de5ec2c449883167
davidob4 <ochincaberegoi@gmail.com>
2023-04-04 02:37:15 +0100

    time stamps

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -17,17 +17,26 @@ const Message = ({ message, loaded }) => {
         }
     }, [message, loaded]);
 
+    function formatDateAndTime(timestamp) {
+        const date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000); // Convert to milliseconds
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const seconds = date.getSeconds().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
+    }
+
     return (
         <div ref={ref}
             className={`message ${message.sendId === currentUser.uid && "owner"}`}
         >
             <div className='message-content'>
                 <p>{message.text}</p>
-                {(message.sendId === currentUser.uid) && <p className='you'>you</p>}
-                {(message.sendId != currentUser.uid) && <p className='you'>{data.user.displayName}</p>}
+                {(message.sendId === currentUser.uid) && <p className='you'>you {formatDateAndTime(message.date)}</p>}
+                {(message.sendId != currentUser.uid) && <p className='you'>{data.user.displayName} {formatDateAndTime(message.date)}</p>}
             </div>
-
-
         </div>
     )
 }
